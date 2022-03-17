0478b98625630e930e341080cec6d02645f0d836
BabulKumar2022 <bb3583400@gmail.com>
2022-03-17 23:49:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 6666;
+const music = {
+  name: 'Khalid', job:'singer'
+}
+const singer = {name:'Andrwo', job:'mordern singer'};
+const singerStyle ={
+  color: 'red',
+  backgroundColor: 'white'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello React Js</h3>
+        </div>
+        <div className="music">
+          <p>Number: {number} </p>
+          <p style = {singerStyle}>Name: {music.name} {music.job}</p>
+          <p style = {{color:'blue', backgroundColor:'yellow'}}>Name: {singer.name} {singer.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
